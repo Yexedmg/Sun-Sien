6984953e47c662a59f26790128deb0e1010dc0f1
⭐️ <yordifaith@MacBook-Pro-3.local>
2026-03-17 18:54:25 +0100

    “update”

--- site-copy.js
+++ site-copy.js
@@ -1,8 +1,8 @@
 (function (global) {
   const SITE_COPY = {
     callWaitNote: {
-      nl: 'Afhalen na 1 uur na het plaatsen van een telefonische bestelling.',
-      en: 'Pickup about 1 hour after placing a phone order.',
+      nl: 'Let op!! Telefonisch bestelling duurt minimaal een uur, wilt u niet zo lang wachten, dan kunt u in de winkel inlopen zonder te bellen , daar wordt je sneller geholpen.',
+      en: 'Please note!! Orders placed by phone take at least an hour. If you don\'t want to wait that long, you can come into the store without calling, and you\'ll be able to order sooner.',
     },
   };
 
